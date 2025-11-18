c3a0f3c57ed396afe14fec52e52c05dd8fa7c7c1
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-11-18 12:37:37 -0300

    add: products DTO

---
+++ src/DTO/create-product.dto.ts
@@ -0,0 +1,33 @@
+import { 
+    IsString,
+    IsNumber,
+    IsOptional,
+    IsBoolean,
+    Min,
+    IsUrl 
+} from "class-validator";
+
+export class CreateProductDto {
+    @IsString()
+    name: string;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsNumber()
+    @Min(0)
+    price: number;
+    
+    @IsString()
+    category: string;
+
+    @IsUrl()
+    @IsOptional()
+    image?: string;
+
+    @IsOptional()
+    @IsBoolean()
+    stock?: boolean;
+
+}

c3c2e15da21a089e93c59fe067f44e721f44dfb2
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-11-27 17:30:22 -0300

    add : services order in progg

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+    constructor (
+        private prisma: PrismaService,
+        private UserService:  UsersService
+    ) {}
+
+    async getAllOrders(){
+        
+    }
+}
